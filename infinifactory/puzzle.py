31b7d01ee5ba84c0fd4a62a4e08543dd95789795
IQuick 143 <IQuick143cz@gmail.com>
2021-03-04 11:11:55 +0100

    Added puzzle loading from file

--- infinifactory/puzzle.py
+++ infinifactory/puzzle.py
@@ -1,15 +1,16 @@
-"""Classes for manipulating Infinifacoty save files.
+"""Classes for manipulating Infinifactory save files.
 """
 import base64
 import io
 
 from typing import List, Tuple
+from .world_blocks import WorldBlocks
 
 from PIL import Image
 
 
 class Puzzle(object):
-    """Class for puzzle in Infinifacoty.
+    """Class for puzzle in Infinifactory.
 
     Puzzle keys:
         Title: Title of the puzzle.
@@ -20,8 +21,24 @@ class Puzzle(object):
         WorldBlocks: Base64-encoded world block data.
     """
 
-    def __init__(self, save_data: bytes):
+    def __init__(self, title="Untitled Python Puzzle", world_blocks=WorldBlocks({}), preview_image: Image=None, is_advanced=True, solved=False):
         """Constructor.
+
+        Args:
+            title: Title of the puzzle
+            world_blocks: WorldBlock object describing the blocks in the puzzle
+            preview_image: PIL Image 
+            is_advanced: Whether the puzzle is made for the advanced puzzle editor, defaults to True, likely should stay that way
+            solved: Whether the puzzle was solved, defaults to False, likely should stay that way
+        """
+        self.title = title
+        self.world_blocks = world_blocks
+        self.preview_image = preview_image
+        self.is_advanced = is_advanced
+        self.solved = solved
+
+    def from_bytes(save_data: bytes):
+        """Loads a puzzle from byte data.
 
         Args:
             save_data: bytes of the puzzle file.
@@ -29,12 +46,24 @@ class Puzzle(object):
         save_lines = save_data.splitlines()
         save_dict = dict(l.split(b' = ') for l in save_lines)
 
-        self.title: str = save_dict[b'Title'].decode('utf-8')
-        self.is_advanced = save_dict[b'IsAdvanced'] == b'True'
-        self.solved = save_dict[b'Solved'] == b'True'
+        title: str = save_dict[b'Title'].decode('utf-8')
+        is_advanced = save_dict[b'IsAdvanced'] == b'True'
+        solved = save_dict[b'Solved'] == b'True'
 
         if b'PreviewImage' in save_dict:
-            self.preview_image = Image.open(io.BytesIO(
-                base64.b64decode(save_dict[b'PreviewImage'])))
+            preview_image = Image.open(io.BytesIO(base64.b64decode(save_dict[b'PreviewImage'])))
         else:
-            self.preview_image = None
+            preview_image = None
+
+        world_blocks = WorldBlocks.from_bytes(base64.b64decode(save_dict[b'WorldBlocks']))
+
+        return Puzzle(title, world_blocks, is_advanced, solved, preview_image)
+
+    def from_file(fp: str):
+        """Loads a puzzle from a file.
+
+        Args:
+            fp: path to a puzzle file.
+        """
+        with open(fp, "rb") as file:
+            return Puzzle.from_bytes(file.read())
